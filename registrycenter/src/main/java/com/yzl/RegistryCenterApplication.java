257f784f5cfdb0a2ac7a00e81ebaea5ed7499b58
yzl <382981935@qq.com>
2019-12-18 01:23:42 +0800

    hystrix加入

--- registrycenter/src/main/java/com/yzl/RegistryCenterApplication.java
+++ registrycenter/src/main/java/com/yzl/RegistryCenterApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@SpringBootApplication//启动类
 @EnableEurekaServer
 public class RegistryCenterApplication {
     public static void main(String[] args) {
